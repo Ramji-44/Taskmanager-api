7ead5a57850b17f19ec278aa1fa38871d8ab4304
Ramji-44 <ramjisanmugam@gmail.com>
2026-05-15 21:51:43 +0530

    added sequelize files

---
+++ dao/config/config.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv")
+const path = require("path")
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env")
+})   // load env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false
+  }
+}

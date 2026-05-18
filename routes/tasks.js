145038cd88b8affd00d6c78d6b2184f05939d99e
Ramji-44 <ramjisanmugam@gmail.com>
2026-05-18 20:59:07 +0530

    imported validationFn in routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const router = express.Router()
+
+const { checkSchema } = require("express-validator")
+const { getByIdVS, postTaskVS, putTaskVS, patchTaskVS, deleteTaskVS } = require("../middleware/taskValidationSchema.js")
+
 const taskController = require("../controllers/tasks.js")
+const validate = require("../middleware/validation.js")
 
 // task route
 router.get('/', taskController.getAllTasks)
-router.get('/:id', taskController.getById)
-router.post('/', taskController.createTask)
-router.put('/:id', taskController.putTask)
-router.patch('/:id', taskController.patchTask)
-router.delete('/:id', taskController.deleteTask)
+router.get('/:id', checkSchema(getByIdVS), validate, taskController.getById)
+router.post('/', checkSchema(postTaskVS), validate, taskController.createTask)
+router.put('/:id', checkSchema(putTaskVS), validate, taskController.putTask)
+router.patch('/:id', checkSchema(patchTaskVS), validate, taskController.patchTask)
+router.delete('/:id', checkSchema(deleteTaskVS), validate, taskController.deleteTask)
 
 module.exports = router

487a7d4f1617026c99bad160eff3035e1df96c6b
Ramji-44 <ramjisanmugam@gmail.com>
2026-05-20 10:09:26 +0530

    updated cors origin

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const taskRoutes = require("./routes/tasks");
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin : "http://localhost:3000"
+}))
 
 app.use('/tasks', taskRoutes)
 
